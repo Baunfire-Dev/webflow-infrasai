e7e527cf99552f5b7a5241e25d1986c16f3ff786
rcuer <rcuer@baunfire.com>
2026-06-11 07:11:20 +0800

    updates to build

--- assets/js/custom/animation.js
+++ assets/js/custom/animation.js
@@ -1,8 +1,35 @@
 (function () {
     baunfire.Animation = {
         init() {
-            
+            this.handleNav();
         },
+
+        handleNav() {
+            const header = document.querySelector("header");
+            if (!header) return;
+
+            let lastScrollY = window.scrollY;
+
+            const updateNavScroll = () => {
+                const currentScrollY = window.scrollY;
+
+                header.classList.toggle("nav-scrolled", currentScrollY > 20);
+
+                if (currentScrollY > lastScrollY) {
+                    header.classList.add("nav-hidden");
+                    header.classList.remove("nav-hidden");
+                } else if (currentScrollY < lastScrollY) {
+                    header.classList.add("nav-hidden");
+                    header.classList.remove("nav-hidden");
+                }
+
+                lastScrollY = currentScrollY;
+            };
+
+            document.addEventListener("scroll", updateNavScroll);
+            window.addEventListener("load", updateNavScroll);
+            updateNavScroll();
+        }
     };
 
     baunfire.addModule(baunfire.Animation);
