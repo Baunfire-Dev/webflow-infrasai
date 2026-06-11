298e89b07d9cf22c993908a8b0fc7a69def3c97b
rcuer <rcuer@baunfire.com>
2026-06-12 04:28:35 +0800

    updates to build

--- assets/js/bundles/core-bundle.js
+++ assets/js/bundles/core-bundle.js
@@ -1,6 +1,7 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { SplitText } from 'gsap/SplitText';
 import Lenis from 'lenis';
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
@@ -13,7 +14,8 @@ gsap.ticker.lagSmoothing(0);
 window.gsap = gsap;
 window.ScrollTrigger = ScrollTrigger;
 window.ScrollToPlugin = ScrollToPlugin;
+window.SplitText = SplitText;
 window.Lenis = Lenis;
 window.__lenis = lenis;
 
-export { gsap, ScrollTrigger, ScrollToPlugin, Lenis };
+export { gsap, ScrollTrigger, ScrollToPlugin, SplitText, Lenis };
