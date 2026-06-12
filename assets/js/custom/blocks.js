4d599f0c6c408f90a9da92c34a26500f47d85bdf
rcuer <rcuer@baunfire.com>
2026-06-12 09:55:29 +0800

    updates to build

--- assets/js/custom/blocks.js
+++ assets/js/custom/blocks.js
@@ -81,6 +81,56 @@
             script();
         },
 
+        typeTextReveal() {
+            const script = () => {
+                const els = document.querySelectorAll("section.hero-homepage");
+                if (!els.length) return;
+
+                els.forEach(self => {
+                    handleVideo(self);
+                    handleEntrance(self);
+                });
+            }
+
+            const handleTextHighlight = (self) => {
+                const mainTitle = self.find("h3");
+                const words = mainTitle.find(".word");
+                if (!words.length) return;
+
+                const mm = gsap.matchMedia();
+
+                mm.add({
+                    isDesktop: `(min-width: 992px)`,
+                    isMobile: `(max-width: 991.98px)`,
+                }, (context) => {
+                    let { isDesktop, isMobile } = context.conditions;
+
+                    const tl = gsap.timeline({
+                        scrollTrigger: {
+                            trigger: mainTitle,
+                            scrub: isDesktop ? 2 : true,
+                            start: isDesktop ? "top 80%" : "top 70%",
+                            end: isDesktop ? "bottom 70%" : "bottom 70%",
+                            // markers: true
+                        },
+                    });
+
+                    words.each(function () {
+                        const subSelf = $(this);
+
+                        tl.to(subSelf, {
+                            backgroundPositionX: 0,
+                            ease: "none",
+                        });
+                    })
+
+                    return () => { }
+                });
+            }
+
+            script();
+        },
+
         accordionTextmedia() {
             const script = () => {
                 const els = document.querySelectorAll("section.accordion-text-media");
