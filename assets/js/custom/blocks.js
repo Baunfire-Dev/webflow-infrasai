e731bc898a52ed347d381f6fa5d95c93173243e5
rcuer <rcuer@baunfire.com>
2026-06-25 04:57:17 +0800

    updates to build

--- assets/js/custom/blocks.js
+++ assets/js/custom/blocks.js
@@ -711,7 +711,7 @@
 
                 if (!textBlock || !anchorsContainer) return;
 
-                const headings = textBlock.querySelectorAll("h3");
+                const headings = textBlock.querySelectorAll("h2");
                 if (!headings.length) return;
 
                 anchorsContainer.innerHTML = "";
